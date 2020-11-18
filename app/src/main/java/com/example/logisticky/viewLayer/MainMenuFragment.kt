46c4564c03306cdd28a5ee6fa2cd2b9e1e323974
Michalcovy <mat.michalec@gmail.com>
2020-11-18 16:47:14 +0100

    setting up git

---
+++ app/src/main/java/com/example/logisticky/viewLayer/MainMenuFragment.kt
@@ -0,0 +1,55 @@
+package com.example.logisticky.viewLayer
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.logisticky.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainMenuFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainMenuFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.settingsButton).setOnClickListener(this)
+        view.findViewById<Button>(R.id.productsButton).setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when(v!!.id){
+            R.id.settingsButton -> navController!!.navigate(R.id.action_mainMenuFragment_to_settingsFragment)
+            R.id.productsButton -> navController!!.navigate(R.id.action_mainMenuFragment_to_productsFragment)
+
+        }
+    }
+
+
+
+}
